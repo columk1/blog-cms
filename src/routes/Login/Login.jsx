1f479efc9ff3677216da883a642863060dace077
Colum Kelly <columk1@users.noreply.github.com>
2023-12-23 22:06:20 -0800

    Add icons to login form

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -5,10 +5,11 @@ const Login = () => {
         This example requires updating your template:
 
         ```
-        <html class="h-full bg-white">
-        <body class="h-full">
+        <html className="h-full bg-white">
+        <body className="h-full">
         ```
       */}
+
       <div className='flex min-h-full flex-1 flex-col justify-center px-6 lg:px-8'>
         <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
           <img
@@ -23,45 +24,57 @@ const Login = () => {
 
         <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
           <form className='space-y-6' action='#' method='POST'>
-            <div>
-              <label htmlFor='email' className='block text-sm font-medium leading-6 text-gray-900'>
-                Email address
-              </label>
-              <div className='mt-2'>
+            <div className='flex flex-col mb-2'>
+              <div className='flex relative '>
+                <span className='rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm'>
+                  <svg
+                    width='15'
+                    height='15'
+                    fill='currentColor'
+                    viewBox='0 0 1792 1792'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path d='M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z'></path>
+                  </svg>
+                </span>
                 <input
-                  id='email'
-                  name='email'
-                  type='email'
-                  autoComplete='email'
-                  required
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  type='text'
+                  id='sign-in-email'
+                  className=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent'
+                  placeholder='Your email'
                 />
               </div>
             </div>
 
-            <div>
-              <div className='flex items-center justify-between'>
-                <label
-                  htmlFor='password'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
-                  Password
-                </label>
-                <div className='text-sm'>
-                  <a href='#' className='font-semibold text-indigo-600 hover:text-indigo-500'>
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className='mt-2'>
+            <div className='flex flex-col mb-6'>
+              <div className='flex relative '>
+                <span className='rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm'>
+                  <svg
+                    width='15'
+                    height='15'
+                    fill='currentColor'
+                    viewBox='0 0 1792 1792'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path d='M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z'></path>
+                  </svg>
+                </span>
                 <input
-                  id='password'
-                  name='password'
                   type='password'
-                  autoComplete='current-password'
-                  required
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  id='sign-in-email'
+                  className=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent'
+                  placeholder='Your password'
                 />
+              </div>
+            </div>
+            <div className='flex items-center mb-6 -mt-4'>
+              <div className='flex ml-auto'>
+                <a
+                  href='#'
+                  className='inline-flex text-xs font-thin text-gray-500 sm:text-sm hover:text-gray-700'
+                >
+                  Forgot Your Password?
+                </a>
               </div>
             </div>
 
@@ -70,17 +83,10 @@ const Login = () => {
                 type='submit'
                 className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
               >
-                Sign in
+                Log in
               </button>
             </div>
           </form>
-
-          <p className='mt-10 text-center text-sm text-gray-500'>
-            Not a member?{' '}
-            <a href='#' className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'>
-              Start a 14 day free trial
-            </a>
-          </p>
         </div>
       </div>
     </>
