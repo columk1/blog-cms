267f614e9f97e7bc13029b4d59092308b97cbabb
Colum Kelly <columk1@users.noreply.github.com>
2024-06-25 00:13:13 -0700

    Create logo icon component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,30 @@
+const Logo = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      id='Layer_2'
+      data-name='Layer 2'
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 51.35 51.35'
+    >
+      <defs>
+        <style>
+          {`
+            .cls-1 {
+              fill: none;
+              stroke-miterlimit: 10;
+          }
+          `}
+        </style>
+      </defs>
+      <g id='Orig_Home' data-name='Orig Home'>
+        <path
+          className='cls-1'
+          d='m36.85.5v36H.85V.5h36ZM7.85,7.5v36h36V7.5H7.85Zm7,7v36h36V14.5H14.85ZM.35,1l50,50L.35,1Z'
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default Logo
